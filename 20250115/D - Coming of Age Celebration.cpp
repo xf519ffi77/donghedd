6d6a01e6a8a0c7b089a93fb28495644f4d30a139
xf519ffi77 <xf519ffi77@outlook.com>
2025-01-16 01:24:33 +0800

    abc388

--- 20250115/D - Coming of Age Celebration.cpp
+++ 20250115/D - Coming of Age Celebration.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 const int N = 500010;
 int n;
-int a[N], d[N], ans[N];
+int a[N], c[N], d[N], ans[N];
 int main()
 {
 	ios::sync_with_stdio(0), cin.tie(0), cout.tie(0);
@@ -11,13 +11,14 @@ int main()
 	for (int i = 1; i <= n; i ++ )
 	{
 		cin >> a[i];
-		d[i] += d[i - 1];
-		ans[i] = a[i] + d[i];
-		int t = min(ans[i], n - i);
-		ans[i] -= t;
-		if (i + 1 <= n) d[i + 1] ++ ;
-		if (i + 1 + t <= n) d[i + 1 + t] -- ;
+		c[i] = c[i - 1] + d[i]; // 差分，c[i]记录前i-1个人中有几个会给第i个人送石头 
+		a[i] += c[i];
+		int t = min(a[i], n - i);
+		a[i] -= t;
+		// 利用差分，[i + 1, i + t + 1]整体++ 
+		d[i + 1] ++ ;
+		d[i + t + 1] -- ;
 	}
-	for (int i = 1; i <= n; i ++ ) cout << ans[i] << ' ';
+	for (int i = 1; i <= n; i ++ ) cout << a[i] << ' ';
 	return 0;
 }
