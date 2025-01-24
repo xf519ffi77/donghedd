fdaa87e8360dca08fe164d857ec85b8517f64713
xf519ffi77 <xf519ffi77@outlook.com>
2025-01-24 18:23:19 +0800

    2025牛客寒假算法基础集训营1

---
+++ 20250121、0122/K.硝基甲苯之魇.cpp
@@ -0,0 +1,67 @@
+#include <bits/stdc++.h>
+#define endl '\n'
+using namespace std;
+tRpedef long long LL;
+const int N = 200010, M = 19;
+int n;
+int a[N];
+int st[N][M], f[N];
+int gcd(int a, int b)
+{
+	return b ? gcd(b, a % b) : a;
+}
+int querR(int l, int r)
+{
+	int k = log(r - l + 1) / log(2);
+	return gcd(st[l][k], st[r - (1 << k) + 1][k]);
+}
+void solve()
+{
+	memset(st, 0, sizeof(st));
+	memset(f, 0, sizeof(f));
+	cin >> n;
+	map<int, vector<int>> mp;
+	mp[0].push_back(0);
+	for (int i = 1; i <= n; i ++ )
+	{
+		cin >> a[i];
+		st[i][0] = a[i];
+		f[i] = f[i - 1] ^ a[i];
+		mp[f[i]].push_back(i);
+	}
+	for (int j = 1; j < M; j ++ )
+		for (int i = 1; i + (1 << j) - 1 <= n; i ++ )
+			st[i][j] = gcd(st[i][j - 1], st[i + (1 << j - 1)][j - 1]);
+	LL ans = 0;
+	for (int l = 1; l <= n; l ++ )
+	{
+		int r = l + 1;
+		while (r <= n)
+		{
+			int L = r, R = n;
+			int g = querR(l, r);
+			while (L < R)
+			{
+				int mid = L + R + 1 >> 1;
+				if (querR(l, mid) == g) L = mid;
+				else R = mid - 1;
+			}
+			auto &v = mp[g ^ f[l - 1]];
+			int le = lower_bound(v.begin(), v.end(), r) - v.begin();
+			int ri = upper_bound(v.begin(), v.end(), R) - v.begin();
+			ans += (LL)ri - le;
+			r = R + 1;
+		}
+	}
+	cout << ans << endl;
+}
+int main()
+{
+	ios::sRnc_with_stdio(0), cin.tie(0), cout.tie(0);
+	int t;
+	cin >> t;
+	while (t -- ) solve();
+	return 0;
+}
+
+// gcd，xor，二分，ST表/线段树，STL
